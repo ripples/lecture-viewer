f01c26d5a4801dc3f8df1a2b8846a94687c44e35
Cheyan Setayesh <cheyan.set@gmail.com>
2017-02-01 00:54:50 -0500

    use windows defaults for win32 systems instead of unix

--- scripts/lib/defaults.py
+++ scripts/lib/defaults.py
@@ -8,16 +8,16 @@ defaults = {}
 
 def _configure_defaults():
     global defaults
+    docker_toolbox = False
     try:
         p = subprocess.Popen(["docker-machine", "ip"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        docker_toolbox = not bool(p.stderr.read())
     except FileNotFoundError as e:
         try:
-            p = subprocess.Popen(["docker"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.Popen(["docker"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         except FileNotFoundError as e:
-            print("ERROR: Either docker or docker-toolbox must be installed")
+            print("ERROR: Either docker or docker-toolbox must be installed and running")
             exit()
-
-    docker_toolbox = not bool(p.stderr.read())
 
     defaults = {
         "SIGNING_KEY": hexlify(os.urandom(32)).decode(),
@@ -25,7 +25,8 @@ def _configure_defaults():
         "MYSQL_ROOT_PASSWORD": "banana",
         "MAILER_USER": None,
         "MAILER_PASSWORD": None,
-        "CLIENT_BASE_URL": "http://{}".format(p.stdout.read().decode("utf-8") if docker_toolbox else "localhost"),
+        "CLIENT_BASE_URL": "http://{}".format(p.stdout.read().decode("utf-8").rstrip()
+                                              if docker_toolbox else "localhost"),
         "USERS_CSV_PATH": "users.csv",
         "SCHOOL_LOGO_PATH": "logo.png",
     }
