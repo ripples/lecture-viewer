62ca0fa594a9cba5087932927ee062f5ed5ade5e
Cheyan Setayesh <cheyan.set@gmail.com>
2017-01-31 21:55:45 -0500

    fix setup script, bump commits

--- scripts/lib/setup_prompt.py
+++ scripts/lib/setup_prompt.py
@@ -1,30 +1,39 @@
+from getpass import getpass
 from pathlib import Path
 from binascii import hexlify
 import os
 import sys
 import shutil
 
-env_file = Path("../.env")
+env_file = Path(".env")
 
 
 def prompt(text):
-    response = input(text)
+    response = input(text + " [y/N]")
     if response == "y":
         return True
     else:
         return False
 
-if env_file.exists():
-    print("File .env already exists!")
-    if prompt("Would you like to create a new one? [y/N]"):
-        env_file.unlink()
-        env_file.touch()
-    else:
-        sys.exit(0)
-
 
 def setup():
-    mysql_root_pw = input("Enter a password for the database. Defaults to 'banana': ") or "banana"
+    if env_file.exists():
+        print("File .env already exists!")
+        if prompt("Would you like to create a new one?"):
+            env_file.unlink()
+            env_file.touch()
+        else:
+            sys.exit(0)
+
+    prod = prompt("Is this a production environment?")
+    node_env = "production" if prod else "development"
+
+    mailer_user = input("Enter a gmail mailer username: ")
+    mailer_password = getpass("Enter a gmail mailer password: ")
+    if not (mailer_user or mailer_password):
+        print("WARNING: both a mailer username and password is needed to send emails")
+
+    mysql_root_pw = getpass("Enter a password for the database. Defaults to 'banana': ") or "banana"
 
     host_media_dir = Path(
         input("Enter a location for media directory relative to the root directory. "
@@ -32,7 +41,7 @@ def setup():
     ).expanduser()
     if not (host_media_dir.exists() and host_media_dir.is_dir()):
         print("{} not found, creating directory".format(host_media_dir))
-        os.system("sudo mkdir -p {}".format(host_media_dir))
+        os.makedirs(host_media_dir)
 
     signing_key = input("Enter a custom signing key (not suggested). Defaults to 64 character random string: ")
     if not signing_key:
@@ -42,7 +51,7 @@ def setup():
         input("Enter a location for school logo to previously provided media directory {}/"
               .format(host_media_dir))).expanduser())
     if school_logo.is_file():
-        shutil.copy((str(school_logo)), "../lv-client/client/src/images/logo.png")
+        shutil.copy((str(school_logo)), "./lv-client/client/src/images/logo.png")
     else:
         print("WARNING: school logo file does not exist")
 
@@ -58,6 +67,7 @@ def setup():
 
     print("Generating .env file")
     env_file.write_text("""SIGNING_KEY={signing_key}
+NODE_ENV={node_env}
 
 # lv-db
 MYSQL_HOSTNAME=lv-db
@@ -74,10 +84,16 @@ USERS_CSV_PATH={users_csv_path}
 # lv-server
 SERVER_HOSTNAME=lv-server
 API_VERSION=v1
+MAILER_USER={mailer_user}
+MAILER_PASSWORD={mailer_password}
 
 # lv-client
 CLIENT_HOSTNAME=lv-client
 CLIENT_BASE_URL={client_url}
 SCHOOL_LOGO_PATH={school_logo}
-""".format(signing_key=signing_key, mysql_root_pw=mysql_root_pw, host_media_dir=host_media_dir,
-           users_csv_path=users_csv_path, client_url=client_url, school_logo=school_logo))
+""".format(signing_key=signing_key, node_env=node_env, mysql_root_pw=mysql_root_pw, host_media_dir=host_media_dir,
+           users_csv_path=users_csv_path, mailer_user=mailer_user, mailer_password=mailer_password,
+           client_url=client_url, school_logo=school_logo))
+
+    if not prod and not Path(host_media_dir, "F16").exists():
+        os.system("sudo mkdir -p {}/F16".format(host_media_dir))
--- scripts/setup.py
+++ scripts/setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-
+import os
 import sys
+
+from pathlib import Path
 
 if sys.version_info < (3, 0):
     raise SystemExit("Sorry, this code need Python 3.0 or higher")
 else:
-    from setup_lib.setup_prompt import setup
+    from lib.setup_prompt import setup
+    try:
+        next(filter(lambda file: file.name == "docker-compose.yml", Path(os.getcwd()).glob("*")))
+    except StopIteration as e:
+        print("ERROR: You must execute this script in the root lecture viewer directory")
+        sys.exit(1)
     setup()
 
