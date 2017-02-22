265a6aaac7a67f10d7852a693222445079e3f3c4
Cheyan Setayesh <cheyan.set@gmail.com>
2017-02-22 02:08:53 -0500

    add auto insert users job

--- scripts/lib/__init__.py
+++ scripts/lib/__init__.py
@@ -28,7 +28,7 @@ def _prompt(text):
 
 
 def _request_input(text, env=None, password=False, with_path=False, display_default=True):
-    default = defaults[env] if env else None
+    default = defaults.get(env) if env else ""
     prompt_text = "{} | Defaults to {}: ".format(text, default) if default and display_default else text
 
     result = (getpass(prompt_text) if password else input(prompt_text)) or default
@@ -43,8 +43,9 @@ def _request_input(text, env=None, password=False, with_path=False, display_defa
 
 def setup():
     def setup_mailer():
-        if not (_request_input("Enter a gmail mailer username: ", env="MAILER_USER") and
-                _request_input("Enter a gmail mailer password: ", env="MAILER_PASSWORD", password=True)):
+        mailer_user = _request_input("Enter a gmail mailer username: ", env="MAILER_USER")
+        mailer_password = _request_input("Enter a gmail mailer password: ", env="MAILER_PASSWORD", password=True)
+        if not (mailer_user and mailer_password):
             print("WARNING: both a mailer username and password is needed to send emails")
 
     def setup_db():
@@ -54,33 +55,44 @@ def setup():
         host_media_dir = _request_input(
             "Enter a location for media directory relative to the root directory.", env="HOST_MEDIA_DIR", with_path=True
         )
+        users_dir = host_media_dir.joinpath('users')
+        resources_dir = host_media_dir.joinpath('resources')
+        inserted_users = users_dir.joinpath('inserted')
+
         if not (host_media_dir.exists() and host_media_dir.is_dir()):
             print("{} not found, creating directory".format(host_media_dir))
             makedirs(str(host_media_dir))
+        if not (users_dir.exists() and users_dir.is_dir()):
+            print("{} not found, creating directory".format(users_dir))
+            makedirs(str(users_dir))
+        if not (resources_dir.exists() and resources_dir.is_dir()):
+            print("{} not found, creating directory".format(resources_dir))
+            makedirs(str(resources_dir))
+        if not (inserted_users.exists() and inserted_users.is_dir()):
+            print("{} not found, creating directory".format(inserted_users))
+            makedirs(str(inserted_users))
 
-        return host_media_dir
+        return host_media_dir, users_dir, resources_dir
 
-    def setup_media_files(host_media_dir):
-        school_logo = host_media_dir.joinpath(_request_input(
-            "Enter a location for school logo path relative to previously provided media directory '{}'".format(
-                host_media_dir), env="SCHOOL_LOGO_PATH", with_path=True))
+    def setup_resources_files(resources_dir):
+        school_logo = resources_dir.joinpath(_request_input(
+            "Enter a location for school logo path relative to resources directory '{}'".format(resources_dir),
+            with_path=True, env="SCHOOL_LOGO_PATH"))
 
         if school_logo.is_file():
             shutil.copy((str(school_logo)), "./lv-client/client/src/images/logo.png")
         else:
             print("WARNING: school logo file does not exist")
 
-        users_csv_path = _request_input(
-            "Enter a location for users csv path relative to previously provided media directory '{}'".format(
-                host_media_dir), env="USERS_CSV_PATH", with_path=True)
-        if not host_media_dir.joinpath(users_csv_path).is_file():
-            print("WARNING: users csv file does not exist")
-
     def setup_miscellaneous():
         envs["NODE_ENV"] = "production" if _prompt("Is this a production environment?") else defaults["NODE_ENV"]
         _request_input("Enter a custom signing key (not suggested). | Defaults to 64 character random string: ",
                        env="SIGNING_KEY", display_default=False)
         _request_input("Enter the host url for the Lecture Viewer.", env="CLIENT_BASE_URL")
+
+    def setup_dev():
+        _request_input("Enter dev email, this should be your umass email: ",
+                       env="DEV_EMAIL", display_default=False)
 
     if env_file.exists():
         print("File .env already exists!")
@@ -93,13 +105,18 @@ def setup():
     setup_miscellaneous()
     setup_mailer()
     setup_db()
-    media_dir = setup_media_dir()
-    setup_media_files(media_dir)
+    media_dir, users_dir, resources_dir = setup_media_dir()
+    setup_resources_files(resources_dir)
+
+    if envs["NODE_ENV"] == "development":
+        setup_dev()
+
     print("Generating .env file")
 
     with env_file.open(mode="w") as file:
-        file.write("""SIGNING_KEY={SIGNING_KEY}
+        formatted_file = """SIGNING_KEY={SIGNING_KEY}
 NODE_ENV={NODE_ENV}
+HOST_MEDIA_DIR={HOST_MEDIA_DIR}
 
 # lv-db
 MYSQL_HOSTNAME=lv-db
@@ -110,8 +127,6 @@ MYSQL_USER=root
 # lv-media
 MEDIA_HOSTNAME=lv-media
 MEDIA_SERVER_PORT=5000
-HOST_MEDIA_DIR={HOST_MEDIA_DIR}
-USERS_CSV_PATH={USERS_CSV_PATH}
 
 # lv-server
 SERVER_HOSTNAME=lv-server
@@ -124,8 +139,12 @@ MAILER_PASSWORD={MAILER_PASSWORD}
 CLIENT_HOSTNAME=lv-client
 CLIENT_PORT=3000
 CLIENT_BASE_URL={CLIENT_BASE_URL}
-SCHOOL_LOGO_PATH={SCHOOL_LOGO_PATH}
-""".format(**envs))
+""".format(**envs)
+        if envs["NODE_ENV"] == "development":
+            formatted_file += """
+DEV_EMAIL={DEV_EMAIL}
+""".format(**envs)
+        file.write(formatted_file)
 
     if not envs["NODE_ENV"] == "production" and not Path(media_dir, "F16").exists():
         mkdir(str(Path(media_dir, "F16")))
--- scripts/lib/defaults.py
+++ scripts/lib/defaults.py
@@ -27,8 +27,8 @@ def _configure_defaults():
         "MAILER_PASSWORD": "",
         "CLIENT_BASE_URL": "http://{}".format(p.stdout.read().decode("utf-8").rstrip()
                                               if docker_toolbox else "localhost"),
-        "USERS_CSV_PATH": "users.csv",
         "SCHOOL_LOGO_PATH": "logo.png",
+        "DEV_EMAIL": ""
     }
 
 
